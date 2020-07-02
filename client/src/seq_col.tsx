c6fe3167866ad670546a5e215be81e961f18fd4d
Jame <coynej1@wit.edu>
2020-07-01 23:07:55 -0400

    refactored sequencer

--- client/src/seq_col.tsx
+++ client/src/seq_col.tsx
@@ -2,41 +2,47 @@ import React, { Component } from 'react';
 
 type colProps = {
     idx: number;
-    rows: boolean[];
+    size : number;
     callback: (colIdx, col) => void;
 }
 
-class SequencerColumn extends Component<colProps> {
+type colState = {
+    filled: boolean[]
+}
 
-    state = {
-        filled: [false,false,false,false],
+class SequencerColumn extends Component<colProps, colState> {
+
+    // state needs to live in the constructor otherwise it won't work
+    constructor(props) {
+        super(props);
+        this.state = {
+            filled: (new Array(props.size)).fill(true),
+        }
     }
 
-    toggleClass(hek : HTMLElement) {
-        if(hek.classList.contains("row"))
-        {
-          hek.classList.remove("row")
-          hek.classList.add("row_filled")
-          this.state.filled[parseInt(hek.innerHTML)] = true;
-          //this.props.rows[parseInt(hek.innerHTML)] = true;
-        } else {
-          hek.classList.remove("row_filled")
-          hek.classList.add("row")
-          this.state.filled[parseInt(hek.innerHTML)] = false;
-          //this.props.rows[parseInt(hek.innerHTML)] = false;
-        }
+    toggleClass(hek : number) {
+        // create temp var to hold state in & update new change
+        var tempState = this.state.filled;
+        tempState[hek] = !tempState[hek];
+        // update state
+        this.setState({filled: tempState})
+        // call callback
         this.props.callback(this.props.idx, this.state.filled);
       }
 
     render(){
-        //default values
-        //this.setState({filled : [false,false,false,false]})
         return(
-            <div className={"column"} key={this.props.idx}>
-                <div className={"row"} onMouseDown={(e: React.FormEvent<HTMLElement>) => {this.toggleClass(e.currentTarget)}}>0</div>
-                <div className={"row"} onMouseDown={(e: React.FormEvent<HTMLElement>) => {this.toggleClass(e.currentTarget)}}>1</div>
-                <div className={"row"} onMouseDown={(e: React.FormEvent<HTMLElement>) => {this.toggleClass(e.currentTarget)}}>2</div>
-                <div className={"row"} onMouseDown={(e: React.FormEvent<HTMLElement>) => {this.toggleClass(e.currentTarget)}}>3</div>
+            <div className={"column"}>
+                {/* render each cell using map function */}
+                {this.state.filled.map((value, index) => 
+                
+                    <div 
+                        //conditionally render filled or not 
+                        className={value ? "row" : "row_filled"}
+                        // when clicked update column state, which forces the component to rerender, then call the callback 
+                        onClick={() => this.toggleClass(index)}
+                    />
+                )}
             </div>
         )
     }
